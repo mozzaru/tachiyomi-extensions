c9ea13071a4e8424c4cb05fc7b5d8b80f0d7b40d
mozzaru <rianyanuar52@gmail.com>
2025-05-12 08:58:35 +0700

    mgkomik: update v2

--- src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
+++ src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
@@ -1,22 +1,15 @@
 package eu.kanade.tachiyomi.extension.id.mgkomik
 
-//import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Headers
 import okhttp3.Request
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
-import okhttp3.*
-import org.jsoup.Jsoup
-//import org.jsoup.nodes.Document
-
-if (responseBody?.contains("Cloudflare") == true) {
-    val document: Document = Jsoup.parse(responseBody)
-    // Ekstrak informasi dari form tantangan
-}
+import kotlin.random.Random
 
 class MGKomik : Madara(
     "MG Komik",
@@ -24,147 +17,31 @@ class MGKomik : Madara(
     "id",
     SimpleDateFormat("dd MMM yy", Locale.US),
 ) {
-    override val useLoadMoreRequest = LoadMoreStrategy.Never
+    override val tagPrefix = "genres/"
+    override val listUrl = "komik/"
+    override val datePattern = "dd MMM yy"
+    override val stylePage = ""
+    override val sourceLocale: Locale = Locale.ENGLISH
 
-    override val useNewChapterEndpoint = false
-
-    override val mangaSubString = "komik"
-
-    override fun headersBuilder() = super.headersBuilder().apply {
-        add("Sec-Fetch-Dest", "document")
-        add("Sec-Fetch-Mode", "navigate")
-        add("Sec-Fetch-Site", "same-origin")
-        add("Upgrade-Insecure-Requests", "1")
-        add("X-Requested-With", randomString((1..20).random())) // added for webview, and removed in interceptor for normal use
+    private fun generateRandomString(length: Int): String {
+        val charset = "HALOGaES.BCDFHIJKMNPQRTUVWXYZ.bcdefghijklmnopqrstuvwxyz0123456789"
+        return (1..length)
+            .map { charset.random() }
+            .joinToString("")
     }
 
-    override val client = network.cloudflareClient.newBuilder()
-        .addInterceptor { chain ->
-            val request = chain.request()
-            val headers = request.headers.newBuilder().apply {
-                removeAll("X-Requested-With")
-            }.build()
-
-            chain.proceed(request.newBuilder().headers(headers).build())
-        }
-        .rateLimit(9, 2)
-        .build()
-
-    // ================================== Popular ======================================
-
-    override fun popularMangaNextPageSelector() = ".wp-pagenavi span.current + a"
-
-    // ================================== Latest =======================================
-
-    override fun latestUpdatesRequest(page: Int): Request =
-        if (useLoadMoreRequest()) {
-            loadMoreRequest(page, popular = false)
-        } else {
-            GET("$baseUrl/$mangaSubString/${searchPage(page)}", headers)
-        }
-
-    // ================================== Search =======================================
-
-    override fun searchRequest(page: Int, query: String, filters: FilterList): Request {
-        filters.forEach { filter ->
-            when (filter) {
-                is GenreContentFilter -> {
-                    val url = filter.toUriPart()
-                    if (url.isBlank()) {
-                        return@forEach
-                    }
-                    return GET(filter.toUriPart(), headers)
-                }
-                else -> {}
-            }
-        }
-        return super.searchRequest(page, query, filters)
-    }
-
-    override fun searchMangaSelector() = "${super.searchMangaSelector()}, .page-listing-item .page-item-detail"
-
-    override fun searchMangaNextPageSelector() = "a.page.larger"
-
-    // ================================ Chapters ================================
-
-    override val chapterUrlSuffix = ""
-
-    // ================================ Filters ================================
-
-    override fun getFilterList(): FilterList {
-        launchIO { fetchGenres() }
-
-        val filters = super.getFilterList().list.toMutableList()
-
-        filters += if (genresList.isNotEmpty()) {
-            listOf(
-                Filter.Separator(),
-                GenreContentFilter(
-                    title = intl["genre_filter_title"],
-                    options = genresList.map { it.name to it.id },
-                ),
-            )
-        } else {
-            listOf(
-                Filter.Separator(),
-                Filter.Header(intl["genre_missing_warning"]),
-            )
-        }
-
-        return FilterList(filters)
-    }
-
-    private class GenreContentFilter(title: String, options: List<Pair<String, String>>) : UriPartFilter(
-        title,
-        options.toTypedArray(),
-    )
-
-    override fun genresRequest() = GET("$baseUrl/$mangaSubString", headers)
-
-    override fun parseGenres(document: Document): List<Genre> {
-        val genres = mutableListOf<Genre>()
-        genres += Genre("All", "")
-        genres += document.select(".row.genres li a").map { a ->
-            Genre(a.text(), a.absUrl("href"))
-        }
-        return genres
-    }
-
-    // =============================== Utilities ==============================
-
-    private fun randomString(length: Int): String {
-        val charPool = ('a'..'z') + ('A'..'Z') + ('.')
-        return List(length) { charPool.random() }.joinToString("")
-    }
-
-object CloudflareBypasser {
-    private const val url = "<https://example.com>"
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val headers = Headers.Builder()
-            .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36")
-            .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-            .add("Accept-Language", "en-US,en;q=0.5")
-            .add("Accept-Encoding", "gzip, deflate, br")
-            .add("Connection", "keep-alive")
+    override fun getRequestHeaders(): Headers {
+        val randomLength = Random.nextInt(13, 21)
+        val randomString = generateRandomString(randomLength)
+        return Headers.Builder()
+            .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+            .add("Accept-Language", "en-US,en;q=0.9,id;q=0.8")
+            .add("Sec-Fetch-Dest", "document")
+            .add("Sec-Fetch-Mode", "navigate")
+            .add("Sec-Fetch-Site", "same-origin")
+            .add("Sec-Fetch-User ", "?1")
             .add("Upgrade-Insecure-Requests", "1")
-            .add("Cache-Control", "max-age=0")
+            .add("X-Requested-With", randomString)
             .build()
-
-        val client = OkHttpClient()
-        val request = Request.Builder()
-            .url(url)
-            .headers(headers)
-            .build()
-
-        val response = client.newCall(request).execute()
-        println(response.body?.string())
-        val cookieJar = CookieJar.NO_COOKIES
-        val client = OkHttpClient.Builder()
-        .cookieJar(cookieJar)
-        .build()
-
     }
-}
 }
